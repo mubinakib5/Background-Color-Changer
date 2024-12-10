fa1c69952aa31fcfaf0171d538c69d1c7161ca42
mubinakib5 <mubinalakib786@gmail.com>
2024-12-10 15:05:55 +0600

    Second Commit

---
+++ bgchanger/tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Ensures Tailwind scans your components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
